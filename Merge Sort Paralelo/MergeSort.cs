dba29702f30231c03abf8957dc21218031528bb8
LUIS ANGEL ALVARADO HERNANDEZ <angel_samsa@alumnos.uaslp.edu.mx>
2020-03-16 17:50:50 -0600

    Commit Merge Sort

---
+++ Merge Sort Paralelo/MergeSort.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Merge_Sort_Paralelo
+{
+    class MergeSort
+    {
+        private int size;
+        private int[] datos;
+        private Thread[] hilos = new Thread[4];
+
+        //Se crea la matriz
+        public MergeSort(int siz)
+        {
+            size = siz;
+            datos = new int[size];
+            llenar();
+        }
+
+        // Se llena la matriz
+        public void llenar()
+        {
+            Random sRan = new Random();
+            int random;
+            for (int i = 0; i < size; i++)
+            {
+                random = sRan.Next(100);
+                datos[i] = random;
+            }
+        }
+
+        public void imprime()
+        {
+            for (int i = 0; i < size; i++)
+                Console.Write(datos[i] + " ");
+            Console.WriteLine(" ");
+        }
+
+        // Método ejecutado por el padre
+        public void ordena()
+        {
+            // Se crean los hilos
+            for (int i = 0; i < 4; i++)
+            {
+                // Se definen los limites del arreglo con los que se trabajara
+                int l = i * (size / 4);
+                int h = (i + 1) * (size / 4) - 1;
+                // Se crea el nuevo hilo
+                hilos[i] = new Thread(() => merge_sort(l, h));
+                hilos[i].Start();
+            }
+
+            // Se reciben los hilos
+            for (int i = 0; i < 4; i++)
+            {
+                while (hilos[i].ThreadState != ThreadState.Stopped) { }
+            }
+
+            // Se ordenan los datos que se acaban de recibir
+            merge(0, (size / 2 - 1) / 2, size / 2 - 1);
+            merge(size / 2, size / 2 + (size - 1 - size / 2) / 2, size - 1);
+            merge(0, (size - 1) / 2, size - 1);
+        }
+
+        public void merge(int l, int m, int h)
+        {
+            // Se crean los arreglos auxiliares
+            int[] izquierdo = new int[m - l + 1];
+            int[] derecho = new int[h - m];
+
+            // Se definen los limites de los arreglos auxiliares
+            int n1 = m - l + 1;
+            int n2 = h - m, i, j;
+
+            // Vaciado de datos en los arreglos auxiliares
+            for (i = 0; i < n1; i++)
+                izquierdo[i] = datos[i + l];
+
+            for (i = 0; i < n2; i++)
+                derecho[i] = datos[i + m + 1];
+
+            int k = l;
+            i = j = 0;
+
+            // Se realiza el merge, de izquierda a derecha, en orden ascendente
+            while (i < n1 && j < n2)
+            {
+                if (izquierdo[i] <= derecho[j])
+                    datos[k++] = izquierdo[i++];
+                else
+                    datos[k++] = derecho[j++];
+            }
+
+            // Se coloca el resto de los datos
+            while (i < n1)
+            {
+                datos[k++] = izquierdo[i++];
+            }
+
+            while (j < n2)
+            {
+                datos[k++] = derecho[j++];
+            }
+        }
+
+        public void merge_sort(int l, int h)
+        {
+            // Se calcula el valor medio
+            int m = l + (h - l) / 2;
+            if (l < h)
+            {
+                merge_sort(l, m);
+                merge_sort(m + 1, h);
+                merge(l, m, h);
+            }
+        }
+    }
+}
---
+++ Merge Sort Paralelo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Merge_Sort_Paralelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IMPORTANTE: El tamaño de la matriz debe ser multiplo de 4
+            int N = 8000000;
+            MergeSort prueba = new MergeSort(N);
+
+            /*Console.WriteLine("Matriz sin Ordenar:");
+            prueba.imprime();*/
+
+            Stopwatch sw = new Stopwatch();
+            
+
+            //Versión Secuencial
+            Console.WriteLine("Ordenamiento Secuencial");
+            sw.Start();
+            prueba.merge_sort(0, N - 1);
+            sw.Stop();
+
+            if (sw.ElapsedMilliseconds <= 0)
+                Console.WriteLine("Operation completed in less than 0 (ms)");
+            else
+                Console.WriteLine("Operation completed in: " + sw.ElapsedMilliseconds + " (ms)");
+            sw.Reset();
+
+            Console.WriteLine("Ordenamiento Paralelo");
+
+            sw.Start();
+            //Versión Multihilos
+            prueba.ordena();
+
+            sw.Stop();
+
+            /*Console.WriteLine("Matriz Ordenada:");
+            prueba.imprime();*/
+
+            Console.WriteLine();
+            if (sw.ElapsedMilliseconds <= 0)
+                Console.WriteLine("Operation completed in less than 0 (ms)");
+            else
+                Console.WriteLine("Operation completed in: " + sw.ElapsedMilliseconds + " (ms)");
+
+            Console.ReadKey();
+        }
+    }
+}
